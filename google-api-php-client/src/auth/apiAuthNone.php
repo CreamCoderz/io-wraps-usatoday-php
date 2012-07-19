db9281bc1c7b811e49e5c47462832c8558ae9158
Neil Mansilla <neil@mansilla.com>
2012-07-19 04:13:38 -0700

    Minor (comments & spaces)

--- google-api-php-client/src/auth/apiAuthNone.php
+++ google-api-php-client/src/auth/apiAuthNone.php
@@ -15,12 +15,6 @@
  * limitations under the License.
  */
 
-/**
- * Do-nothing authentication implementation, use this if you want to make un-authenticated calls
- * @author Chris Chabot <chabotc@google.com>
- * @author Chirag Shah <chirags@google.com>
- */
-
 /*
  * This file has been modified by Mashery, Inc.
  * 
@@ -52,6 +46,11 @@
  * @author Neil Mansilla <neil@mashery.com>
  */
 
+/**
+ * Do-nothing authentication implementation, use this if you want to make un-authenticated calls
+ * @author Chris Chabot <chabotc@google.com>
+ * @author Chirag Shah <chirags@google.com>
+ */
 class apiAuthNone extends apiAuth {
   public $key = null;
 
@@ -60,31 +59,32 @@ class apiAuthNone extends apiAuth {
     if (!empty($apiConfig['developer_key'])) {
       $this->setDeveloperKey($apiConfig['developer_key']);
     }
-        if (!empty($apiConfig['developer_secret'])) {
-                $this->setDeveloperSecret($apiConfig['developer_secret']);
-        }
+    if (!empty($apiConfig['developer_secret'])) {
+      $this->setDeveloperSecret($apiConfig['developer_secret']);
+    }
   }
 
   public function setDeveloperKey($key) {$this->key = $key;}
+  public function setDeveloperSecret($secret) {$this->secret = $secret;}
 
   public function sign(apiHttpRequest $request) {
-  	global $apiConfig;
+    global $apiConfig;
     $sig = "";
     if ($this->key) {
         if ($this->secret) {
             $sig = md5($this->key . $this->secret . (string)time());
     }
-      $request->setUrl(
+    $request->setUrl(
         $request->getUrl() . 
         ((strpos($request->getUrl(), '?') === false) ? '?' : '&') .
         $apiConfig['key_name'].'='.urlencode($this->key) . 
         (($sig) ? ('&' . $apiConfig['signature_name'] . '=' . urlencode($sig)) : '')
-      );
-        /*
-         * Mod above - static "key" parameter name changed
-         * to global $apiConfig variable "key_name" add signature
-         * if secret exists.
-         */
+    );
+    /*
+     * Mod above - static "key" parameter name changed
+     * to global $apiConfig variable "key_name" add signature
+     * if secret exists.
+     */
     }
     return $request;
   }
